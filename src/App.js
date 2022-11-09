356f7c3a352501760d78d15fd849d479d6deb968
prabhat poddar <pk041222@gmail.com>
2022-11-09 22:21:12 +0530

    new

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/File/Navbar';
 import AllRoutes from './components/Routs/AllRoutes';
+import Footer from './components/File/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <AllRoutes/>
+      <Footer/>
       
     </div>
   );
