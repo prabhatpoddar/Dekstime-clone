010e874dcb711c31e6cf5bb42684cf5ae2aea8d9
prabhat poddar <pk041222@gmail.com>
2022-11-10 19:13:42 +0530

    new

--- src/components/File/Slider.jsx
+++ src/components/File/Slider.jsx
@@ -1,93 +1,93 @@
-import React, { useRef } from 'react'
+import React, { useRef } from "react";
 import {
-    Drawer,
-    DrawerBody,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerOverlay,
-    DrawerContent,
-    DrawerCloseButton,
-    useDisclosure,
-    Button,
-    Input,
-    Image,
-    ButtonGroup,
-    Flex,
-    Grid,
-    Divider
-
-  } from '@chakra-ui/react'
-  import { AiOutlineMenu } from 'react-icons/ai';
-import { Link } from 'react-router-dom';
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+  Button,
+  Input,
+  Image,
+  ButtonGroup,
+  Flex,
+  Grid,
+  Divider,
+} from "@chakra-ui/react";
+import { AiOutlineMenu } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 export default function Slider() {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const btnRef = useRef()
-  
-    return (
-      <>
-        <Button ref={btnRef} bg="transparent" onClick={onOpen}>
-         <AiOutlineMenu fontSize="30px"/>
-        </Button>
-        <Drawer
-          isOpen={isOpen}
-          placement='left'
-          onClose={onClose}
-          finalFocusRef={btnRef}
-         
-        >
-          <DrawerOverlay />
-          <DrawerContent>
-            <DrawerCloseButton />
-            <DrawerHeader><Image
-            
-            src="https://desktime.com/assets/design/dist/assets/Logo-Dark-Text-Without-Padding.svg"
-          /></DrawerHeader>
-  
-            <DrawerBody>
-            <Grid gap={4} alignItems="center" justifyContent="center" fontSize="25px">
-            <Link to="/" >
-              <>Demo</>
-            </Link>
-            <Divider/>
-            
-            <Link to="/">
-              <>Feature</>
-            </Link>
-            <Divider/>
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = useRef();
 
-            <Link to="/">
-              <>Pricing</>
-            </Link>
-            <Divider/>
+  return (
+    <>
+      <Button ref={btnRef} bg="transparent" onClick={onOpen}>
+        <AiOutlineMenu fontSize="30px" />
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>
+            <Image src="https://desktime.com/assets/design/dist/assets/Logo-Dark-Text-Without-Padding.svg" />
+          </DrawerHeader>
 
-            <Link to="/">
-              <>About us</>
-            </Link>
-            <Divider/>
+          <DrawerBody>
+            <Grid
+              gap={4}
+              alignItems="center"
+              justifyContent="center"
+              fontSize="25px"
+            >
+              <Link to="/">
+                <>Demo</>
+              </Link>
+              <Divider />
 
-            <Link to="/">
-              <>FAQ</>
-            </Link>
-            <Divider/>
+              <Link to="/">
+                <>Feature</>
+              </Link>
+              <Divider />
 
-            <Link to="/">
-              <>Blog</>
-            </Link>
-            <Divider />
+              <Link to="/">
+                <>Pricing</>
+              </Link>
+              <Divider />
 
-           
+              <Link to="/">
+                <>About us</>
+              </Link>
+              <Divider />
+
+              <Link to="/">
+                <>FAQ</>
+              </Link>
+              <Divider />
+
+              <Link to="/">
+                <>Blog</>
+              </Link>
+              <Divider />
             </Grid>
-            </DrawerBody>
-  
-            <DrawerFooter>
-              <Button variant='outline' mr={3} onClick={onClose}>
-                Login
-              </Button>
-              <Button colorScheme='blue'>Signup</Button>
-            </DrawerFooter>
-          </DrawerContent>
-        </Drawer>
-      </>
-    )
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant="outline" mr={3} onClick={onClose}>
+              Login
+            </Button>
+            <Button colorScheme="blue">Signup</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
 }
